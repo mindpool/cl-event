aa1f0481e2ad3a719bf1b782238bd8cc0b73ebac
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-14 21:33:26 -0700

    Added more notes.

--- test.lisp
+++ test.lisp
@@ -20,6 +20,14 @@
   ;                                 read; the callback should have the assert
   ;                                 in it
   ;--- TODO (oubiwann@mindpool.io): get a pipe or something similar for lisp
+  ;                                 hrm, no pipes in lisp; we can get a stream
+  ;                                 and then extract an fd... we'll need to
+  ;                                 create two stream, one for reading and one
+  ;                                 for writing... I don't know about streams
+  ;                                 that would support reading and writing like
+  ;                                 Python os.pipe, though... if we can get
+  ;                                 this, however, then we can use
+  ;                                 extract-stream-handle(s)
   ;--- TODO (oubiwann@mindpool.io): create an event with the cb and the fd,
   ;                                 seeing the event type as EV_READ
   ;--- TODO (oubiwann@mindpool.io): write text to the pipe
